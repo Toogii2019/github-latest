81d00a2cb5026ca12cf163a2204735b661b20102
Toogii2019 <dashdava@uw.edu>
2020-05-09 22:06:10 -0700

    Completed lesson06 activity

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import sys
-import json
-
 import requests
 
 # Use Like python githubber.py JASchilz
 # (or another user name)
 
+def get_event_timestamp(username, event_num=1):
+    response = requests.get("https://api.github.com/users/{}/events".format(username))
+    events = response.json()
+    return events[0].get('created_at')
+
 if __name__ == "__main__":
-    username = sys.argv[1]
+    help = 'Example: python main.py {username}'
+    try:
+        username = sys.argv[1]
+    except IndexError:
+        sys.exit(help)
+    timestamp = get_event_timestamp(username, event_num=1)
+    print(timestamp)
+
 
     # TODO:
     #
